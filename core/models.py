5081ab8c14bfeb0b57f00793a387219354a9c793
satishkojushrestha <54971497+satishkojushrestha@users.noreply.github.com>
2022-02-12 22:14:12 +0545

    QR Code generator created

--- core/models.py
+++ core/models.py
@@ -1,6 +1,11 @@
-from tkinter.tix import Tree
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+#for generating qr code
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image, ImageDraw
+from django.conf import settings
 
 
 class User(AbstractUser):
@@ -37,6 +42,29 @@ class Ingredient(models.Model):
     supplier = models.ForeignKey(Supplier, on_delete=models.RESTRICT)    
     time_stamp = models.DateField(auto_now_add=True)
     quantity = models.IntegerField()
+    qr = models.ImageField(upload_to='qr_codes', blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.pk)
+        # return self.name
+
+    def save(self, *args, **kwargs):
+        db_count = Ingredient.objects.count()
+        if db_count == 0:
+            get_pk = 1
+        else:
+            last_pk = Ingredient.objects.last()
+            get_pk = str(last_pk.pk+1)
+            # print(get_pk)
+        qr_url = f'{self.name}/{get_pk}'
+        qrcode_img = qrcode.make(qr_url)
+        qr_size = qrcode_img.pixel_size
+        canvas = Image.new('RGB', (qr_size+8, qr_size+5), 'white')
+        draw = ImageDraw.Draw(canvas)
+        canvas.paste(qrcode_img)
+        fname = f'qr_code-{self.name}'+'.png'
+        buffer = BytesIO()
+        canvas.save(buffer,'PNG')
+        self.qr.save(fname, File(buffer), save=False)
+        canvas.close()
+        super(Ingredient, self).save(*args, **kwargs)
